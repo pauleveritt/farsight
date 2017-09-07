0d4c1c9f6048562281a12b867ff73c25c8c346fa
Paul Everitt <pauleveritt@me.com>
2017-09-07 18:04:56 -0400

    3 routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React, {Component} from 'react';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
 import './App.css';
 import ProductList from "./ProductList";
 
-class App extends Component {
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
+
+class ProductStore extends Component {
     constructor (props) {
         super(props)
         this.state = {
@@ -35,6 +56,48 @@ class App extends Component {
                     onProductSelect={this.handleProductSelect.bind(this)}
                 />
             </div>
+        )
+    }
+}
+
+class App extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            selectedProducts: [],
+            products: [
+                {id: 1, name: 'AirMax 90', brand: 'Nike'},
+                {id: 2, name: 'Yeezy', brand: 'Adidas'},
+                {id: 3, name: 'Classic', brand: 'Reebok'},
+            ]
+        }
+    }
+
+    handleProductSelect (product) {
+        this.setState(prevState => {
+            return {
+                selectedProducts: prevState.selectedProducts.concat(product)
+            }
+        });
+    }
+
+
+    render () {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/topics">Products</Link></li>
+                    </ul>
+                    <hr/>
+
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/topics" component={ProductStore}/>
+                </div>
+            </Router>
         );
     }
 }
