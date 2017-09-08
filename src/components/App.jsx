9b2436f65bbae26c874510c6d54bfc22af163c66
Paul Everitt <pauleveritt@me.com>
2017-09-08 15:48:05 -0400

    Fixed the non-closing. Add bookmarks to the menu.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,149 +12,115 @@ import {indigo900} from 'material-ui/styles/colors'
 
 import MainRoutes from '../routes'
 
+import './App.css'
+
 
 class App extends Component {
-  static propTypes = {
-    // react-router 4:
-    match:    PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history:  PropTypes.object.isRequired
-  }
+    static propTypes = {
+        match: PropTypes.object.isRequired,
+        location: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired
+    }
 
-  static contextTypes = {}
+    static contextTypes = {}
 
-  state = {
-    appName:      appConfig.APP_NAME,
-    drawerOpened: false,
-    drawerMenus:  appConfig.DRAWER.menus
-  }
+    state = {
+        appName: appConfig.APP_NAME,
+        drawerOpened: false,
+        drawerMenus: appConfig.DRAWER.menus
+    }
 
-  render() {
-    const {
-      drawerOpened,
-      drawerMenus,
-      appName
-    } = this.state
+    render () {
+        const {
+            drawerOpened,
+            drawerMenus,
+            appName
+        } = this.state
 
-    return (
-      <div>
-        <Drawer
-          open={drawerOpened}
-          docked={true}
-          onRequestChange={this.handlesOnDrawerRequestChange}
-          style={{zIndex: 1000}}>
-          <div style={{backgroundColor:indigo900, height: '160px'}} />
-          {
-            drawerMenus.map(
-              ({id, title, routeName}, menuIdx) => (
-                <MenuItem
-                  key={menuIdx}
-                  onTouchTap={this.routeTo(routeName)}>
-                  {title}
-                </MenuItem>
-              )
-            )
-          }
-        </Drawer>
-        <AppBar
-          title={appName}
-          onLeftIconButtonTouchTap={this.handlesOnLeftIconButtonTouchTap}
-          iconElementRight={
-            <IconMenu
-              iconButtonElement={
-                <IconButton>
-                  <MoreVertIcon color={'#FFFFFF'} />
-                </IconButton>
-              }
-              targetOrigin={{horizontal: 'right', vertical: 'top'}}
-              anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
-              {
-                drawerMenus.map(
-                  ({id, title, routeName}, menuIdx) => (
-                    <MenuItem
-                      key={menuIdx}
-                      primaryText={title}
-                      onTouchTap={this.routeTo(routeName)}
-                    />
-                  )
-                )
-              }
-            </IconMenu>
-          }
-        />
-        <MainRoutes />
-      </div>
-    )
-  }
+        return (
+            <div>
+                <Drawer
+                    open={drawerOpened}
+                    docked={false}
+                    onRequestChange={this.handlesOnDrawerRequestChange}
+                    style={{zIndex: 1000}}>
+                    {
+                        drawerMenus.map(
+                            ({id, title, routeName}, menuIdx) => (
+                                <MenuItem
+                                    key={menuIdx}
+                                    onTouchTap={this.routeTo(routeName)}>
+                                    {title}
+                                </MenuItem>
+                            )
+                        )
+                    }
+                </Drawer>
+                <AppBar
+                    title={appName}
+                    onLeftIconButtonTouchTap={this.handlesOnLeftIconButtonTouchTap}
+                    iconElementRight={
+                        <IconMenu
+                            iconButtonElement={
+                                <IconButton>
+                                    <MoreVertIcon color={'#FFFFFF'}/>
+                                </IconButton>
+                            }
+                            targetOrigin={{
+                                horizontal: 'right',
+                                vertical: 'top'
+                            }}
+                            anchorOrigin={{
+                                horizontal: 'right',
+                                vertical: 'top'
+                            }}>
+                            {
+                                drawerMenus.map(
+                                    ({id, title, routeName}, menuIdx) => (
+                                        <MenuItem
+                                            key={menuIdx}
+                                            primaryText={title}
+                                            onTouchTap={this.routeTo(routeName)}
+                                        />
+                                    )
+                                )
+                            }
+                        </IconMenu>
+                    }
+                />
+                <MainRoutes/>
+            </div>
+        )
+    }
 
-  openDrawer = () => {
-    this.setState({drawerOpened: true})
-  }
+    openDrawer = () => {
+        this.setState({drawerOpened: true})
+    }
 
-  closeDrawer = () => {
-    this.setState({drawerOpened: false})
-  }
+    closeDrawer = () => {
+        this.setState({drawerOpened: false})
+    }
 
-  toggleDrawer = () => {
-    const { drawerOpened } = this.state
-    this.setState({drawerOpened: !drawerOpened})
-  }
+    toggleDrawer = () => {
+        const {drawerOpened} = this.state
+        this.setState({drawerOpened: !drawerOpened})
+    }
 
-  routeTo = routeName => event => {
-    event.preventDefault()
-    const { history } = this.props
-    history.push({pathname: routeName})
-  }
+    routeTo = routeName => event => {
+        event.preventDefault()
+        const {history} = this.props
+        this.toggleDrawer()
+        history.push({pathname: routeName})
+    }
 
-  handlesOnDrawerRequestChange = (open) => {
-    this.setState({drawerOpened: open})
-  }
+    handlesOnDrawerRequestChange = (open) => {
+        this.setState({drawerOpened: open})
+    }
 
-  handlesOnLeftIconButtonTouchTap = (event) => {
-    event.preventDefault()
-    this.toggleDrawer()
-  }
+    handlesOnLeftIconButtonTouchTap = (event) => {
+        event.preventDefault()
+        this.toggleDrawer()
+    }
 }
 
 export default withRouter(App)
-
-
-// import React from 'react'
-//
-// import {
-//     BrowserRouter as Router,
-//     Redirect,
-//     Route,
-//     Switch
-// } from 'react-router-dom'
-// import {withRouter} from 'react-router-dom'
-//
-// import './App.css'
-//
-//
-// import HomePage from "./HomePage"
-// import AboutPage from "./AboutPage"
-// import ListBookmarks from "./ListBookmarks"
-// import AddBookmark from "./AddBookmark"
-// import NotFound from "./NotFound"
-// import NavHeader from "./NavHeader"
-//
-// const App = () => (
-//     <Router>
-//         <div>
-//             <NavHeader/>
-//             <Switch>
-//                 <Route exact path="/" component={HomePage}/>
-//                 <Route path="/about" component={AboutPage}/>
-//                 <Redirect from="/old-match" to="/about"/>
-//                 <Route path="/bookmarks/add"
-//                        component={AddBookmark}/>
-//                 <Route path="/bookmarks"
-//                        component={ListBookmarks}/>
-//                 <Route component={NotFound}/>
-//             </Switch>
-//         </div>
-//     </Router>
-// )
-//
-// export default withRouter(App)
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
 import App from './components/App'
-import Homepage from "./components/HomePage"
-import AboutPage from "./components/AboutPage"
-import NotFound from "./components/NotFound"
+import Homepage from './components/HomePage'
+import AboutPage from './components/AboutPage'
+import NotFound from './components/NotFound'
+import AddBookmark from './components/AddBookmark';
+import ListBookmarks from './components/ListBookmarks';
 
 const Routes = () => {
     return (
         <Switch path="/" component={App}>
             <Route exact path="/" component={Homepage}/>
             <Route path="/about" component={AboutPage}/>
+            <Redirect from="/old-match" to="/about"/>
+            <Route path="/bookmarks/add"
+                   component={AddBookmark}/>
+            <Route path="/bookmarks"
+                   component={ListBookmarks}/>
             <Route component={NotFound}/>
         </Switch>
     )
