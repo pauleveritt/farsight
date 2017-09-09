a67e6cc392c72283a450452e426281db71018842
Paul Everitt <pauleveritt@me.com>
2017-09-09 11:42:58 -0400

    Fix a bad import

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import {Grid, Row, Col} from 'react-flexbox-grid'
-import {RaisedButton} from 'material-ui';
 
 class Homepage extends React.Component {
     render () {
         return (
-            <Grid fluid>
-                <Row>
-                    <Col xs={6} md={3}>
-                        Hello, world!
-                    </Col>
-                    <Col xs={6} md={3}>
-                        Hello, world!
-                        <RaisedButton label="Material UI"/>
-                    </Col>
-                </Row>
-            </Grid>        )
+            <h1>Home Page</h1>
+        )
     }
 }
 
