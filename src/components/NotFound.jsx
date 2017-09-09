498a955d2c90a415320901a2532020e84157f4f4
Paul Everitt <pauleveritt@me.com>
2017-09-09 09:13:57 -0400

    Redirect to add bookmark on popup open

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const NotFound = ({location}) => (
-    <div>
-        <h3>No match for <code>{location.pathname}</code></h3>
-    </div>
-)
+const NotFound = (props) => {
+    const location = props.location
+    const history = props.history
+
+    if (location.pathname === '/index.html') {
+        // Looks like we're in the Chrome extension, let's redirect to
+        // the "right" place. Add Bookmark for now.
+        history.push('/bookmarks/add')
+    }
+    return (
+        <div>
+            <h3>No match for <code>{location.pathname}</code></h3>
+        </div>
+    )
+}
 
 export default NotFound
